083e2af10678fa5fa735338099ada53be0032609
Karen Dombroski <karen.dombroski@marsbioimaging.com>
2023-05-23 11:05:02 +1200

    don't blindly define WIN32_LEAN_AND_MEAN
    Signed-off-by: Karen Dombroski <karen.dombroski@marsbioimaging.com>

--- loguru.cpp
+++ loguru.cpp
@@ -126,8 +126,12 @@
 	#ifndef _WIN32_WINNT
 		#define _WIN32_WINNT 0x0502
 	#endif
-	#define WIN32_LEAN_AND_MEAN
-	#define NOMINMAX
+	#ifndef WIN32_LEAN_AND_MEAN
+		#define WIN32_LEAN_AND_MEAN
+	#endif
+	#ifndef NOMINMAX
+		#define NOMINMAX
+	#endif
 	#include <windows.h>
 #endif
 
